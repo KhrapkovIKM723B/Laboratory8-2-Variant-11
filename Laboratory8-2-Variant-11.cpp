af49433936588b2dc2fb605f2934a9a5ce440fc2
KhrapkovIKM723B <Ihor.Khrapkov@infiz.khpi.edu.ua>
2024-03-18 21:11:13 +0200

    Ex.2

--- Laboratory8-2-Variant-11.cpp
+++ Laboratory8-2-Variant-11.cpp
@@ -2,43 +2,29 @@
 //
 
 #include <iostream>
+#include <string>
 #include <vector>
 
 int main() {
-    std::vector<double> nums = { -2,3,-5,-4,1,5,2,-1,-3,-7};
-    double positiveSum = 0.0;
-    int negativeCount = 0;
-    double sumBetweenPositives = 0.0;
-    int positiveIndices[2] = { -1, -1 }; // Зберігає індекси першого та другого додатніх елементів
+    int n;
+    std::cout << "Введіть ціле число n (n < 999999): ";
+    std::cin >> n;
 
-    // а) Сума додатних і кількість від’ємних елементів масиву
-    for (int i = 0; i < nums.size(); ++i) {
-        if (nums[i] > 0) {
-            positiveSum += nums[i];
-            if (positiveIndices[0] == -1) {
-                positiveIndices[0] = i; // Знайшли перший додатній елемент
-            }
-            else if (positiveIndices[1] == -1) {
-                positiveIndices[1] = i; // Знайшли другий додатній елемент
-            }
-        }
-        else if (nums[i] < 0) {
-            ++negativeCount;
-        }
+    std::string number = std::to_string(n);
+    std::vector<int> digits(6, 0); // Масив з 6 елементів, заповнений нулями
+
+    // Заповнення масиву цифрами числа у зворотному порядку
+    int numLength = number.length();
+    for (int i = 0; i < numLength; ++i) {
+        digits[i] = number[numLength - i - 1] - '0'; // Перетворення символу на число
     }
 
-    // б) Сума елементів масиву, розташованих між першим і другим додатними елементами
-    if (positiveIndices[0] != -1 && positiveIndices[1] != -1) {
-        for (int i = positiveIndices[0] + 1; i < positiveIndices[1]; ++i) {
-            sumBetweenPositives += nums[i];
-        }
-
+    // Виведення масиву
+    std::cout << "Масив, заповнений цифрами числа у зворотному порядку: ";
+    for (int digit : digits) {
+        std::cout << digit << " ";
     }
-
-
-    std::cout << "Сума додатних елементів: " << positiveSum << std::endl;
-    std::cout << "Кількість від'ємних елементів: " << negativeCount << std::endl;
-    std::cout << "Сума елементів між першим і другим додатніми: " << sumBetweenPositives << std::endl;
+    std::cout << std::endl;
 
     return 0;
 }
